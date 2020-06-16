c248022a8e779fa9f3ef783ccff75985d2e4bf0b
vasyl.bhd <vasyl.bhd@gmail.com>
2020-06-16 21:27:44 +0300

    Make ApiClient fancy AF

--- src/api/achiApiClient.js
+++ src/api/achiApiClient.js
@@ -10,15 +10,21 @@ const endpoints = {
     search: "/search",
     global: "/global",
 };
+
+const composePath = (paths) => paths.join('/')
+const alsUrl = cId => composePath([endpoints.classes, cId, endpoints.als])
+const proceduresUrl = (cId, alsId) => composePath([alsUrl(cId), alsId, endpoints.procedures])
+const blocksUrl = (classId, alsId, pId) => composePath([proceduresUrl(classId, alsId), pId, endpoints.blocks])
+const blocksByIdUrl = (bId) => composePath([endpoints.blocks, bId])
+
 const getAchi = url => get(PREFIX + url);
 
 export const getClasses = () => getAchi(endpoints.classes)
+export const getAls = classId => getAchi(alsUrl(classId))
+export const getProcedures = (cId, alId) => getAchi(proceduresUrl(cId, alId))
+export const getBlocks = (cId, alId, pId) => getAchi(blocksUrl(cId, alId, pId))
+export const getBlocksById = bId => getAchi(blocksByIdUrl(bId))
 export const globalSearch = name => getAchi(`${endpoints.search}?name=${name}`)
-export const getAls = classId => getAchi(`${endpoints.classes}/${classId}/${endpoints.als}`)
-export const getProcedures = (cId, alId) => getAchi(`${endpoints.classes}/${cId}/${endpoints.als}/${alId}/${endpoints.procedures}`)
-export const getBlocks = (cId, alId, pid) => getAchi(`${endpoints.classes}/${cId}/${endpoints.als}/${alId}/${endpoints.procedures}/${pid}/${endpoints.blocks}`)
-export const getBlocksById = bId => getAchi(`${endpoints.blocks}/${bId}`)
-
 export const blockSearch = (blockId, procedureId, alName) => {
     const blockQueryParam = blockId ? `blockId=${blockId}` : "";
     const procedureQueryParam = procedureId ? `procedureId=${procedureId}` : "";
